a8dacb10e063bbd107dbd817025dbea1a19f42a5
Anomali007 <franzese007@gmail.com>
2019-09-27 22:52:45 -0700

    Country examples and app.js main function made. Updated ReadME to address startup.

---
+++ Public/app.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+let countries = require('./../examples/countries.json');
+
+// Check if data is available
+if (countries) {
+  // For loop through countries
+  for (let i = 0; i < countries.length; i++) {
+    // Set variable for current country and country name
+    let country = countries[i];
+    let countryName = countries[i]["country"];
+    // Check if sections are available
+    if (country.sections.length > 0) {
+      console.log('country sections available for ', countryName);
+      // Loop through sections
+      for (let i = 0; i <= country["sections"].length; i++) {
+        // Define Section title and Section Subtitle
+        let sectionTitle = country["sections"][i]["title"];
+        let sectionSubtitle = country["sections"][i]["subtitle"];
+        // Check section for tables
+        console.log("sectionTitle: ", sectionTitle, "sectionSubtitle: ", sectionSubtitle)
+      }
+    } else {
+      console.log('No Country Sections Found')
+    }
+  }
+
+
+
+} else {
+  console.log('No Country Data Found. ')
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # markdown-table-generator
+
+Running 'npm start' from the root directory will begin the file writing process.
+
+The data to be written is taken from 'examples/countries.json'
+
+An example of the output code is in 'examples/countryProfile.html'
+
+All the output from the node process will end up in the 'build' folder
